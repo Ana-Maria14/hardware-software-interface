d42203af2d85aa99cd286f998235954f559d5474
Ana-Maria14 <anamariacatrina14@gmail.com>
2025-03-16 20:59:23 +0200

    c-by: Ana-Maria14 <anamariacatrina14@gmail.com>

--- labs/lab-02/tasks/pointers/support/pointers.c
+++ labs/lab-02/tasks/pointers/support/pointers.c
@@ -6,34 +6,31 @@
 
 int my_strcmp(const char *s1, const char *s2)
 {
-	/**
-	 * TODO: implement function
-	 */
-	(void) s1;
-	(void) s2;
-
-	return 0x420;
+	while(*s1 && *s2) {
+		if(*s1 != *s2) {
+			return *s1 - *s2;
+		}
+		s1++;
+		s2++;
+	}
+	return *s1 - *s2;
 }
 
 void *my_memcpy(void *dest, const void *src, size_t n)
 {
-	/**
-	 * TODO: implement function
-	 */
-	(void) dest;
-	(void) src;
-	(void) n;
+	unsigned char *d = (unsigned char*) dest;
+	const unsigned char *s = (const unsigned char*) src;
 
+	while(n--) {
+		*d++ = *s++;
+	}
 	return dest;
 }
 
 char *my_strcpy(char *dest, const char *src)
 {
-	/**
-	 * TODO: implement function
-	 */
-	(void) dest;
-	(void) src;
+	char* first = dest;
+	while((*dest++ = *src++));
 
-	return dest;
+	return first;
 }
